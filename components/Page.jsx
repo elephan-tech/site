9a77d59a9175621aefdb94b07f376895ad2d23e6
Ivan Romero <cromero2@ameren.com>
2020-02-09 14:54:39 -0600

    Favicon

--- components/Page.jsx
+++ components/Page.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Head from 'next/head'
 import Navbar from './Navbar'
+import etLogo from '../public/etLogo.ico'
 
 const Page = ({ title, description, children }) => {
   return (
     <>
       <Head>
         <title>{`ElephanTech - ${title || 'home'}`}</title>
-        <link rel="shortcut icon" href="static/etLogo.png" />
+        <link rel="shortcut icon" href={etLogo}/>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="description" content={description} />
         <meta name="og:title" property="og:title" content={`ElephanTech - ${title || 'home'}`} />
