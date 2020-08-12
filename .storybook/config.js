610802db68cf4be089847f568ff9cb3b0cf3b9b2
elbgi <elbgi@monsanto.com>
2020-08-11 21:11:32 -0500

    remove packages

--- .storybook/config.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { configure } from '@storybook/react';
-import { addDecorator } from '@storybook/react';
-import { ThemeProvider } from "styled-components";
-import theme from '../src/styles/theme';
-
-// automatically import all files ending in *.stories.tsx
-configure(require.context('../src', true, /\.stories\.jsx?$/), module);
-addDecorator(storyFn => <ThemeProvider theme={theme}>{storyFn()}</ThemeProvider>)
--- .storybook/manager.js
+++
@@ -1,7 +0,0 @@
-import { addons } from '@storybook/addons';
-import { themes } from '@storybook/theming';
-import elephanTech from './elephantech';
-
-addons.setConfig({
-  theme: elephanTech
-});
