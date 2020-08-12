efba881629029be83ef8a32cc5721256ae358f55
elbgi <elbgi@monsanto.com>
2020-08-11 21:13:51 -0500

    theme stuff

--- src/theme.js
+++ src/theme.js
@@ -1,4 +1,6 @@
-const theme = {
+const getShadow = (v, color) => `5px ${v ? (v * 10) : 10}px 10px 0px ${color}50`
+
+const theme = (dark) => ({
   unit: (size) => `${size ? (size * 8) : 8}px`,
   font: {
     size: {
@@ -13,55 +15,33 @@ const theme = {
   },
   palette: {
     primary: {
-      main: '#357081',
-      light: '#659eb0',
-      dark: '#004554'
+      main: !dark ? '#357081' : '#C4C4C4',
+      light: !dark ? '#659eb0' : '#4f5b62',
+      dark: !dark ? '#004554' : '#000a12'
     },
     secondary: {
-      main: '#C4C4C4',
-      light: '#f7f7f7',
-      dark: '#949494',
+      main: !dark ? '#C4C4C4' : '#004554',
+      light: !dark ? '#f7f7f7' : '#3a7080',
+      dark: !dark ? '#4f5b62' : '#357081',
     },
     accent: {
-      main: '#65AD8F',
-      light: '#96dfbf',
-      dark: '#357d62'
+      main: !dark ? '#65AD8F' : '#23b4bd',
+      light: !dark ? '#96dfbf' : '#659eb0',
+      dark: !dark ? '#357d62' : '#004554'
     },
-    black: '#000000',
+    glow: {
+      contrast: getShadow(1, !dark ? '#C4C4C4' : '#23b4bd'),
+      primary: getShadow(1, !dark ? '#23b4bd' : '#4f5b62')
+    },
+    contrast: !dark ? '#231f20' : '#23b4bd',
     white: '#ffffff',
-    warning: '#EDC73F',
-    error: '#E70A0A',
-    success: '#64B215',
-    info: '#23b4bd'
+    background: !dark ? '#ffffff' : '#263238'
   },
-  dimensions: {
-    sm: { flexDirection: 'column' },
-    md: { flexDirection: 'column' },
-    lg: { flexDirection: 'row' }
-  },
-  shadow: (v) => `5px ${v ? (v * 10) : 10}px 10px 0px ${theme.palette.secondary.dark}50`
-}
-
-export const darkTheme = {
-  ...theme,
-  palette: {
-    primary: {
-      main: '#263238',
-      light: '#4f5b62',
-      dark: '#000a12'
-    },
-    secondary: {
-      main: '#004554',
-      light: '#3a7080',
-      dark: '#001e2b',
-    },
-    black: '#000000',
-    white: '#ffffff',
-  },
-  warning: '#fffa72',
-  error: '#ff5739',
-  success: '#98e54f',
-  info: '#68e7ef'
-}
+  warning: '#EDC73F',
+  error: '#E70A0A',
+  success: '#64B215',
+  info: '#23b4bd',
+  shadow: (v) => getShadow(v, !dark ? '#949494' : '#4f5b62')
+});
 
 export default theme
